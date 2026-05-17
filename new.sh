26217830c72d7a91cb07aa12dbee194f9094c4c2
BusecanAntonio <xmaxxantono@yahoo.com>
2026-05-17 20:40:38 +0300

    new comands for memegraph

--- new.sh
+++ new.sh
@@ -1,2 +1,4 @@
 ollama run llama3
 #MATCH p=(n)-[r]->(m) RETURN p LIMIT 500;
+#MATCH (st:Student) ... MATCH (s:ESCOSkill) ... MERGE (st)-[:HAS_SKILL]->(s)
+#MATCH (j:JobApplication) ... MATCH (s:ESCOSkill) ... MERGE (j)-[:REQUIRES_SKILL]->(s)
